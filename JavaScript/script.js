abe11fb44aaa0000ff43974a8595889d1b9bd807
goofySam47 <horn.samuel07@gmail.com>
2024-09-07 22:49:02 +0200

    Hinzufügen von JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,29 @@
+// Variablen die in diesem Programm verwendet werden
+let Notizen = document.getElementById('Notizen');
+let addBtn = document.getElementById('addBtn');
+
+
+// Anfang des Programmes
+addBtn.addEventListener('click', function(){
+    let Status = checkInputUser(Notice);
+
+    switch(Status)
+    {
+        case 1:
+            alert('Bitte gib eine Notiz ein!');
+            break;
+    }
+});
+
+// Funktion, um das Inputfeld zu kontrollieren
+function checkInputUser(Notice)
+{
+    if(Notice.value.trim() === '')
+    {
+        return 1;
+    }
+    else
+    {
+        return 2;
+    }
+}
